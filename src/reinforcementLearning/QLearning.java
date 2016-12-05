f9ddfc70221f967dfa546742f8bfe12c733d3699
Monica Thornton <monicasuecooper@hotmail.com>
2016-12-05 15:56:22 -0700

    Added preliminary print statements for QLearning and Value Iteration

--- src/reinforcementLearning/QLearning.java
+++ src/reinforcementLearning/QLearning.java
@@ -1,5 +1,40 @@
 package reinforcementLearning;
 
-public abstract class QLearning extends Driver {
+import java.util.logging.Level;
+
+public class QLearning extends Driver {
+	private String[][] track;
+	private String algoName;
+	private String trackName;
+	private String crashName;
 	
+	public QLearning(String[][] track, String algoName, String trackName, String crashName) {
+		this.track = track;
+		this.algoName = algoName;
+		this.trackName = trackName;
+		this.crashName = crashName;
+		
+		printTrackInfo(algoName, trackName, crashName);
+	}
+	
+	
+	
+	
+	void train() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	void test() {
+		// TODO Auto-generated method stub
+	}
+	
+	
+	
+	
+	public void printTrackInfo(String algoName, String trackName,
+			String crashName) {
+		super.get_logger().log(Level.INFO, "Running " + algoName + " on " + trackName + " with " + crashName);
+		super.get_logger().log(Level.INFO, "");		
+	}	
 }
--- src/reinforcementLearning/ValueIteration.java
+++ src/reinforcementLearning/ValueIteration.java
@@ -1,8 +1,42 @@
 package reinforcementLearning;
 
-public abstract class ValueIteration extends Driver {
-	
+import java.util.logging.Level;
+
+import reinforcementLearning.Driver;
+
+public class ValueIteration extends Driver {
+	private String[][] track;
+	private String algoName;
+	private String trackName;
+	private String crashName;
+
+	public ValueIteration(String[][] track, String algoName, String trackName, String crashName) {
+		this.track = track;
+		this.algoName = algoName;
+		this.trackName = trackName;
+		this.crashName = crashName;
 		
+		printTrackInfo(algoName, trackName, crashName);
+	}
+	
+	
+	void train() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	void test() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
+	public void printTrackInfo(String algoName, String trackName,
+			String crashName) {
+		super.get_logger().log(Level.INFO, "Running " + algoName + " on " + trackName + " with " + crashName);
+		super.get_logger().log(Level.INFO, "");		
+	}	
 		
 }
 
