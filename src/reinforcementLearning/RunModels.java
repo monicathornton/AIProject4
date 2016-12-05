59b72148c7c7a93a6c0d9c8ae5c16ee854e068a6
Monica Thornton <monicasuecooper@hotmail.com>
2016-12-05 16:45:51 -0700

    Made so user has to choose valid choice for algo, crash and track

--- src/reinforcementLearning/RunModels.java
+++ src/reinforcementLearning/RunModels.java
@@ -1,16 +1,8 @@
 package reinforcementLearning;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-
-import java.io.FileWriter;
 
 public class RunModels {
 
@@ -31,39 +23,69 @@ public class RunModels {
 		// gets location of track (for sending to TrackBuilder)
 		String trackFilePath = "";
 
-		// gives the user algorithm choices
-		System.out
-				.println("Please pick from one of the following reinforcement learning algorithms");
-		System.out
-				.println("To race a car using Value Iteration, please type 'v'");
-		System.out.println("To race a car using Q-learning, please type 'q'");
+		// gives the user algorithm choices, forces them to choose from the list
+		while (!algoChoice.equalsIgnoreCase("v") && !algoChoice.equalsIgnoreCase("q")) {
+			
+			System.out.println("***Please pick from one of the following algorithms***");
+			System.out.println("To race a car using Value Iteration, please type 'v'");
+			System.out.println("To race a car using Q-learning, please type 'q'");
 
-		// holds the user's choice of algorithm
-		algoChoice = in.nextLine().toLowerCase();
+			// holds the user's choice of algorithm
+			algoChoice = in.nextLine().toLowerCase();			
+			
+			if (!algoChoice.equalsIgnoreCase("v") && !algoChoice.equalsIgnoreCase("q")) {
+				System.out.println(algoChoice + " is an invalid choice for algorithm, please choose again.");
+				System.out.println();
+			} else {
+				System.out.println();
+			}
+		} // end while: user has picked a valid algo choice
 
-		// gives the user a series of track choices
-		System.out.println("Please pick a racetrack");
-		System.out.println("To select the L-track please type 'l'");
-		System.out.println("To select the O-track please type 'o'");
-		System.out.println("To select the R-track please type 'r'");
-		System.out.println("To select the training track please type 't'");
-		// holds the user's choice of track
-		trackChoice = in.nextLine().toLowerCase();
 
-		// gives the user the crash choices
-		System.out.println("Please indicate the severity of crashing");
-		System.out
-				.println("If crashing places the car back at the crash site type 'b'");
-		System.out
-				.println("If crashing places the car back at the beginning type w'");
-		System.out.println("Type 'x' to exit");
+		// gives the user a series of track choices, forces them to choose from the list
+		while (!trackChoice.equalsIgnoreCase("l") && !trackChoice.equalsIgnoreCase("o") && !trackChoice.equalsIgnoreCase("r") && !trackChoice.equalsIgnoreCase("t")) {		
+			System.out.println("***Please pick a racetrack***");
+			System.out.println("To select the L-track please type 'l'");
+			System.out.println("To select the O-track please type 'o'");
+			System.out.println("To select the R-track please type 'r'");
+			System.out.println("To select the training track please type 't'");
+		
+			// holds the user's choice of track
+			trackChoice = in.nextLine().toLowerCase();
 
-		// holds the user's choice of crash variation
-		crashChoice = in.nextLine().toLowerCase();
+			if (!trackChoice.equalsIgnoreCase("l") && !trackChoice.equalsIgnoreCase("o") && !trackChoice.equalsIgnoreCase("r") && !trackChoice.equalsIgnoreCase("t")) {
+				System.out.println(trackChoice + " is an invalid choice for the racetrack, please choose again.");
+				System.out.println();
+			} else {
+				System.out.println();
+			}
+		
+		} // end while: user has picked a valid track choice
+		
+		
+		// gives the user the crash choices, forces them to choose from the list
+		while (!crashChoice.equalsIgnoreCase("b") && !crashChoice.equalsIgnoreCase("w") && !crashChoice.equalsIgnoreCase("x")) {
+			
+			System.out.println("***Please indicate the severity of crashing***");
+			System.out.println("If crashing places the car back at the crash site type 'b'");
+			System.out.println("If crashing places the car back at the beginning type w'");
+			System.out.println("Type 'x' to exit the program");
 
-		// associates the user's choices with names of algorithm, track and
-		// crash type
-		// TODO: make sure algo choice, track choice and crash choice are valid choices
+			// holds the user's choice of crash variation
+			crashChoice = in.nextLine().toLowerCase();
+
+			if (!crashChoice.equalsIgnoreCase("b") && !crashChoice.equalsIgnoreCase("w") && !crashChoice.equalsIgnoreCase("x")) {
+				System.out.println(crashChoice + " is an invalid choice for crash variation, please choose again.");
+				System.out.println();
+			} else {
+				System.out.println();
+			}
+			
+			
+		} // end while: user has picked a valid crash choice
+		
+		
+		// associates the user's choices with names of algorithm, track & crash type
 		switch (algoChoice) {
 		case "v":
 			algoName = "Value Iteration";
@@ -95,11 +117,16 @@ public class RunModels {
 		case "w":
 			crashName = "restart at the beginning.";
 			break;
+		case "x":
+			System.out.println("Exiting program.");
+			in.close();
+			System.exit(0);			
 		}
 
-		// TODO: call starting at racetracks data file
+		// get the file path for the race track of interest
 		trackFilePath = getTrack(trackName);
 
+		// reads the racetrack in from the file
 		TrackBuilder track = new TrackBuilder(trackFilePath); 
 		String[][] racetrack = track.buildTrack();
 		
@@ -107,27 +134,19 @@ public class RunModels {
 			// call algo with track data
 			ValueIteration v = new ValueIteration(racetrack, algoName, trackName, crashName);
 			
-			
 			// call racecar in each algo (crashName tells us which crash variant to use) - pass data and crashName (or corresponding Boolean)
 			
 		} else if (algoChoice.equals("q")) {
 			// call algo with track data
 			QLearning q = new QLearning(racetrack, algoName, trackName, crashName);			
 			
-			
-		} else {
-			// user chose to exit the program or typed their choice incorrectly
-			System.out.println("Exiting program.");
-			in.close();
-			System.exit(0);
-		}
+		} 
+
 		// closes the scanner
 		in.close();
-
 	}
 
-	// gets the location of the data for classification (training and test sets
-	// are built in the parser)
+	// gets the location of the racetrack 
 	public static String getTrack(String trackName) {
 		// gets the os for the computer this program is run on
 		String os = System.getProperty("os.name").toLowerCase();
@@ -148,44 +167,8 @@ public class RunModels {
 		}
 
 		// calls the file chooser, returns the updated file path
-		//System.out.println("Select Racetrack Location");
-		//filePathData = callFileChooser(filePathData);
 		System.out.println("Racetrack Location: " + filePathData);
 		System.out.println();
 		return filePathData;
-	}
-
-	// calls a window with a pop up box that lets the user choose their exact
-	// file location (with input from file string that gives user's home
-	// directory.
-	public static String callFileChooser(String filePath) {
-		// builds a JFrame
-		JFrame frame = new JFrame("Folder Selection Pane");
-		// string to score the path
-		String thisPath = "";
-
-		// JFrame look and feel
-		frame.setPreferredSize(new Dimension(400, 200));
-		frame.setLayout(new FlowLayout());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JButton button = new JButton("Select Folder");
-
-		// sets up the file chooser
-		JFileChooser fileChooser = new JFileChooser();
-		// uses file path as a starting point for file browsing
-		fileChooser.setCurrentDirectory(new File(filePath));
-		// choose only from directories
-		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		int fileChosen = fileChooser.showOpenDialog(null);
-
-		// returns either the file path, or nothing (based on user choice)
-		if (fileChosen == JFileChooser.APPROVE_OPTION) {
-			File selectedFile = fileChooser.getSelectedFile();
-			thisPath = selectedFile.getAbsolutePath();
-			return thisPath;
-		} else {
-			return null;
-		}
 	}
 }
