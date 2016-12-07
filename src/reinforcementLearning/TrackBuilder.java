d8826a089060317373b741ae65c01619ed5c2f73
Monica Thornton <monicasuecooper@hotmail.com>
2016-12-06 20:25:44 -0700

    Moved printing from TrackBuilder

--- src/reinforcementLearning/TrackBuilder.java
+++ src/reinforcementLearning/TrackBuilder.java
@@ -8,11 +8,7 @@ import java.io.IOException;
 public class TrackBuilder {
 	// location of the racetrack
 	private String dataFileLocation;
-	
-	// gets the number of rows and cols from the data file (first line, comma separated)
-	private int rows = 0; 
-	private int cols = 0;
-	
+		
 	// 2-d array to store racetrack
 	private String[][] racetrack;
 	
@@ -25,6 +21,10 @@ public class TrackBuilder {
 	public String[][] buildTrack() {
 		// read in data
 		String currentLine = null;
+		
+		// gets the number of rows and cols from the data file (first line, comma separated)
+		int rows = 0; 
+		int cols = 0;
 		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(new File(
@@ -60,112 +60,7 @@ public class TrackBuilder {
 		
 		return racetrack;
 	}
-	
-	// print out the racetrack
-	public void printTrack() {
-		for (int i = 0; i < racetrack.length; i++) {
-			for (int j = 0; j < racetrack[i].length; j++) {
-				System.out.print(racetrack[i][j]);	
-			}
-			System.out.println();
-		}		
-	}
 
-	// builds arrays to store locations of characters of interest
-	public int[][] buildArray(String thisChar, int arraySize) {
-		
-		// at each row, element 0 is xLoc, element 1 is yLoc
-		int[][] localArray = new int[arraySize][2];
-				
-		int laIndex = 0;
-		
-		for (int i = 0; i < racetrack.length; i++) {
-			for (int j = 0; j < racetrack[i].length; j++) {
-				if(racetrack[i][j].equalsIgnoreCase(thisChar)) {
-					localArray[laIndex][0] = i;
-					localArray[laIndex][1] = j;
-					laIndex++;
-				}
-			} 
-		} // end for: have stored x and y loc every time thisChar has been encountered		
-					
-		return localArray;
-	}
-	
-	// gets the locations for the starting line
-	public int[][] getStartLocs() {
-		// array to store start locations (xLoc element 0, yLoc element 1)
-		int[][] startLocs;
-		// keep track of the number of times a start loc is encountered
-		int startLocsCount = 0;
-		
-		String findChar = "S";
-		
-		for (int i = 0; i < racetrack.length; i++) {
-			for (int j = 0; j < racetrack[i].length; j++) {
-				if(racetrack[i][j].equalsIgnoreCase(findChar)) {
-					startLocsCount++;
-				}
-			} 
-		} // end for: have counted the number of times the start loc symbol (S) has been encountered		
-		
-		startLocs = new int[startLocsCount][startLocsCount]; 
-		
-		startLocs = buildArray(findChar, startLocsCount);
-		
-		// get all start locations in original racetrack
-		return startLocs;
-	}
-	
-	// gets the locations for the finish line
-	public int[][] getFinishLocs() {
-		// array to store finish locations (xLoc element 0, yLoc element 1)
-		int[][] finishLocs;
-		// keep track of the number of times a start loc is encountered
-		int finishLocsCount = 0;
-		
-		String findChar = "F";
-		
-		for (int i = 0; i < racetrack.length; i++) {
-			for (int j = 0; j < racetrack[i].length; j++) {
-				if(racetrack[i][j].equalsIgnoreCase(findChar)) {
-					finishLocsCount++;
-				}
-			} 
-		} // end for: have counted the number of times the start loc symbol (S) has been encountered		
-		
-		finishLocs = new int[finishLocsCount][finishLocsCount]; 
-		
-		finishLocs = buildArray(findChar, finishLocsCount);
-			
-		// get all start locations in original racetrack
-		return finishLocs;
-	}	
-	
-	// gets the locations for the walls
-	public int[][] getWallLocs() {
-		// array to store finish locations (xLoc element 0, yLoc element 1)
-		int[][] wallLocs;
-		// keep track of the number of times a start loc is encountered
-		int wallLocsCount = 0;
-		
-		String findChar = "#";
-		
-		for (int i = 0; i < racetrack.length; i++) {
-			for (int j = 0; j < racetrack[i].length; j++) {
-				if(racetrack[i][j].equalsIgnoreCase(findChar)) {
-					wallLocsCount++;
-				}
-			} 
-		} // end for: have counted the number of times the start loc symbol (S) has been encountered		
-		
-		wallLocs = new int[wallLocsCount][wallLocsCount]; 
-		
-		wallLocs = buildArray(findChar, wallLocsCount);
-			
-		// get all start locations in original racetrack
-		return wallLocs;
-	}		
 }
 
 				
