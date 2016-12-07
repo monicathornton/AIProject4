985d307586c4beb2c47ae1e5981f8c169cc53ee6
Monica Thornton <monicasuecooper@hotmail.com>
2016-12-06 20:23:54 -0700

    Started playing with car moving in Value Iteration.

--- src/reinforcementLearning/ValueIteration.java
+++ src/reinforcementLearning/ValueIteration.java
@@ -1,5 +1,6 @@
 package reinforcementLearning;
 
+import java.io.IOException;
 import java.util.logging.Level;
 
 import reinforcementLearning.Driver;
@@ -9,14 +10,45 @@ public class ValueIteration extends Driver {
 	private String algoName;
 	private String trackName;
 	private String crashName;
-
-	public ValueIteration(String[][] track, String algoName, String trackName, String crashName) {
+	
+	private int positionX = 7;
+	private int positionY = 1;
+	private int prevPosX = 0;
+	private int prevPosY = 0;
+	
+	
+	public ValueIteration(String[][] track, String algoName, String trackName, String crashName) throws IOException {
 		this.track = track;
 		this.algoName = algoName;
 		this.trackName = trackName;
 		this.crashName = crashName;
 		
+		
+		
+		
+		
+		
+		
+		
 		printTrackInfo(algoName, trackName, crashName);
+		Car c = new Car(track);
+		// newX, newY, oldX, oldY
+		track = c.moveCar(track, positionX, positionY, prevPosX, prevPosY);
+		prevPosX = positionX;
+		prevPosY = positionY;
+		printTrack(track);
+		positionY++;
+		
+		track = c.moveCar(track, positionX, positionY, prevPosX, prevPosY);
+		prevPosX = positionX;
+		prevPosY = positionY;
+		printTrack(track);
+		positionY++;
+		
+		track = c.moveCar(track, positionX, positionY, prevPosX, prevPosY);
+		printTrack(track);		
+		prevPosX = positionX;
+		prevPosY = positionY;
 	}
 	
 	
@@ -31,7 +63,7 @@ public class ValueIteration extends Driver {
 	}
 	
 
-	
+	// info for printing out for samples run
 	public void printTrackInfo(String algoName, String trackName,
 			String crashName) {
 		super.get_logger().log(Level.INFO, "Running " + algoName + " on " + trackName + " with " + crashName);
