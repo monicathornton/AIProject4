60c9c6c9a42dd5ba6fd5d3bf0013aec3c0d11eb8
Janette Marie Rounds <janette.rounds@msu.montana.edu>
2016-12-15 01:25:20 -0700

    Qlearning up to date

--- results/analysis.r
+++ results/analysis.r
@@ -6,25 +6,35 @@ qlo = read.csv("qlo.csv",sep=",",header=F)
 qlr = read.csv("qlrB.csv",sep=",",header=F)
 qlrW = read.csv("qlrW.csv",sep=",",header=F)
 
-vil = read.csv("vill.csv",sep=",",header=F)
+vil = read.csv("vil.csv",sep=",",header=F)
 vio = read.csv("vio.csv",sep=",",header=F)
 vir = read.csv("vir.csv",sep=",",header=F)
 virW = read.csv("virW.csv",sep=",",header=F)
 
 ##########################
 # add column names
-colnames(qll) <- c("Iteration", "Moves", "Crashes")
-colnames(qlo) <- c("Iteration", "Moves", "Crashes")
-colnames(qlr) <- c("Iteration", "Moves", "Crashes")
-colnames(qlrW) <- c("Iteration", "Moves", "Crashes")
+colnames(qll) <- c("Moves", "Crashes")
+colnames(qlo) <- c("Moves", "Crashes")
+colnames(qlr) <- c("Moves", "Crashes")
+colnames(qlrW) <- c("Moves", "Crashes")
 
-colnames(vil) <- c("Iteration", "Moves", "Crashes")
-colnames(vio) <- c("Iteration", "Moves", "Crashes")
-colnames(vir) <- c("Iteration", "Moves", "Crashes")
-colnames(virW) <- c("Iteration", "Moves", "Crashes")
+colnames(vil) <- c("Moves", "Crashes")
+colnames(vio) <- c("Moves", "Crashes")
+colnames(vir) <- c("Moves", "Crashes")
+colnames(virW) <- c("Moves", "Crashes")
 
 
 #########################
 # Test imputation
 
-t.test(houseknnT$Accuracy, houseknnF$Accuracy, var.equal = FALSE)
+t.test(qll$Moves, vil$Moves, var.equal = FALSE)
+t.test(qll$Crashes, vil$Crashes, var.equal = FALSE)
+
+t.test(qlo$Moves, vio$Moves, var.equal = FALSE)
+t.test(qlo$Crashes, vio$Crashes, var.equal = FALSE)
+
+t.test(qlr$Moves, vir$Moves, var.equal = FALSE)
+t.test(qlr$Crashes, vir$Crashes, var.equal = FALSE)
+
+t.test(qlrW$Moves, virW$Moves, var.equal = FALSE)
+t.test(qlrW$Crashes, virW$Crashes, var.equal = FALSE)
